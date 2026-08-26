e3ab9b5610c7c238223f0b731f1a26a67450b09b
Andrés Muñoz <andres2078@gmail.com>
2026-08-26 15:40:16 -0500

    feat: add product routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,8 @@
 import express from 'express';
+import router from './router';
 
 const server = express();
 
-// Routing
-server.get('/', (req, res) => {
-  
-  const datos = [
-    { id: 1, nombre: 'Andrés' },
-    { id: 2, nombre: 'Juan' }
-  ]
-
-  res.json(datos)
-})
-
+server.use('/api/products', router)
 
 export default server;
