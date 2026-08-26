422a94f8bf3196285a170ffcd23c602ef3d5c990
Andrés Muñoz <andres2078@gmail.com>
2026-08-26 17:50:27 -0500

    feat: configure database connection with environment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,19 +5,19 @@ import db from './config/db';
 // Conectar a base de datos
 async function connectDB() {
   try {
-    await db.authenticate()
-    db.sync()
-    console.log('Conexión exitosa a la BD')
+    await db.authenticate();
+    db.sync();
+    console.log('Conexión exitosa a la BD');
   } catch (error) {
-    console.log(error)
-    console.log('Hubo un error al conectar a la BD')
+    console.log(error);
+    console.log('Hubo un error al conectar a la BD');
   }
 }
 
-connectDB()
+connectDB();
 
 const server = express();
 
-server.use('/api/products', router)
+server.use('/api/products', router);
 
 export default server;
