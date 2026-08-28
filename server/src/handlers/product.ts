d3e319d5f6f37e07a2c34b93af59314199a1a7c3
Andrés Muñoz <andres2078@gmail.com>
2026-08-27 20:24:30 -0500

    refactor: move product validatin to router

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,18 +1,8 @@
 import { Request, Response } from 'express';
-import { check, validationResult } from 'express-validator';
+import { validationResult } from 'express-validator';
 import Product from '../models/Product.model';
 
 export const createProduct = async (req: Request, res: Response) => {
-
-  // Validación
-  await check('name')
-    .notEmpty().withMessage('El nombre del Producto no puede ir vacío')
-    .run(req)
-  await check('price')
-    .isNumeric().withMessage('Valor no válido')
-    .notEmpty().withMessage('El precio del Producto no puede ir vacío')
-    .custom(value => value > 0).withMessage('Precio no válido')
-    .run(req)
 
   let errors = validationResult(req)
   if (!errors.isEmpty()) {
