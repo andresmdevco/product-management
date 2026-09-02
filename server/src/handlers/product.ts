f415c841eeaa06e83ce10699b1a81447ffa83163
Andrés Muñoz <andres2078@gmail.com>
2026-09-01 21:34:26 -0500

    feat: add partial product update endpoint

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -54,3 +54,19 @@ export const updateProduct = async (req: Request, res: Response) => {
 
   res.json({ data: product });
 };
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(400).json({
+      error: 'Producto No Encontrado',
+    });
+  }
+
+  // Actualizar 
+  product.availability = !product.dataValues.availability
+  await product.save()
+  res.json({ data: product });
+};
