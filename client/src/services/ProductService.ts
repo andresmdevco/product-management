e2b9bc932471e8098f33ccd52135ed2d7a640a92
Andrés Muñoz <andres2078@gmail.com>
2026-09-07 17:46:53 -0500

    feat: send product data to REST API

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,4 +1,5 @@
 import { safeParse } from 'valibot';
+import axios from 'axios';
 import { DraftProductSchema } from '../types';
 
 type ProductData = {
@@ -12,7 +13,11 @@ export async function addProduct(data: ProductData) {
       price: +data.price,
     });
     if (result.success) {
-      
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;                 
+      const { data } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price,
+      });
     } else {
       throw new Error('Datos no válidos');
     }
