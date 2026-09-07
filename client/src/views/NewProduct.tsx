f7169c53f24ada62c6f63fadc870ebcea8492221
Andrés Muñoz <andres2078@gmail.com>
2026-09-07 16:15:30 -0500

    feat: retrieve and validate form data

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,12 +1,26 @@
-import { Link, Form } from 'react-router-dom';
+import { Link, Form, useActionData } from 'react-router-dom';
 
-export async function action() {
-  console.log('desde action...');
+export async function action({ request }) {
+  const data = Object.fromEntries(await request.formData());
+
+  let error = '';
+  if (Object.values(data).includes('')) {
+    error = 'Todos los campos con obligatorios';
+  }
+
+  if (error.length) {
+    return error;
+  }
 
   return {};
 }
 
 export default function NewProduct() {
+
+  const error = useActionData()
+
+  console.log(error)
+
   return (
     <>
       <div className="flex justify-between">
