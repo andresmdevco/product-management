27aa81175ae1d5658532b2472ebba6be08ed9853
Andrés Muñoz <andres2078@gmail.com>
2026-09-07 16:55:10 -0500

    feat: add product creation service

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,5 +1,6 @@
 import { Link, Form, useActionData, type ActionFunctionArgs } from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
+import { addProduct } from '../services/ProductService';
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
@@ -8,10 +9,11 @@ export async function action({ request }: ActionFunctionArgs) {
   if (Object.values(data).includes('')) {
     error = 'Todos los campos con obligatorios';
   }
-
   if (error.length) {
     return error;
   }
+
+  addProduct(data);
 
   return {};
 }
