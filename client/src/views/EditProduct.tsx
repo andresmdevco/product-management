ce875413c1f8c73ba3c896eb5f6089c643dbf706
Andrés Muñoz <andres2078@gmail.com>
2026-09-08 18:09:07 -0500

    feat: pass product data to edit form

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -1,4 +1,11 @@
-import { Link, Form, useActionData, type ActionFunctionArgs, redirect } from 'react-router-dom';
+import {
+  Link,
+  Form,
+  useActionData,
+  type ActionFunctionArgs,
+  redirect,
+  useLocation,
+} from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
 import { addProduct } from '../services/ProductService';
 
@@ -20,6 +27,9 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function EditProduct() {
   const error = useActionData() as string;
+  const { state } = useLocation();
+
+  console.log(state);
 
   return (
     <>
@@ -46,6 +56,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Nombre del Producto"
             name="name"
+            defaultValue={state.product.name}
           />
         </div>
         <div className="mb-4">
@@ -58,6 +69,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Precio Producto. ej. 200, 300"
             name="price"
+            defaultValue={state.product.price}
           />
         </div>
         <input
