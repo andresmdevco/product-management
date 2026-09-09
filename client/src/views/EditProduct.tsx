1372a10563f5240686b22392f90c1cdf5fe8492e
Andrés Muñoz <andres2078@gmail.com>
2026-09-08 21:25:07 -0500

    feat: add availability field to edit form

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -38,6 +38,11 @@ export async function action({ request, params }: ActionFunctionArgs) {
   }
 }
 
+const availabilityOptions = [
+  { name: 'Disponible', value: true },
+  { name: 'No Disponible', value: false },
+];
+
 export default function EditProduct() {
   const product = useLoaderData() as Product;
   const error = useActionData() as string;
@@ -70,6 +75,7 @@ export default function EditProduct() {
             defaultValue={product.name}
           />
         </div>
+
         <div className="mb-4">
           <label className="text-gray-800" htmlFor="price">
             Precio:
@@ -83,6 +89,25 @@ export default function EditProduct() {
             defaultValue={product.price}
           />
         </div>
+
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="availability">
+            Disponibilidad:
+          </label>
+          <select
+            id="availability"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map((option) => (
+              <option key={option.name} value={option.value.toString()}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
